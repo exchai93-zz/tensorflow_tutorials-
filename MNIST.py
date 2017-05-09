136106ae0ba734a2092b8c96b93d4cbbdd70c84a
Emily Chai <echai93@gmail.com>
2017-05-09 16:17:35 +0100

    training model using cross entropy

--- MNIST.py
+++ MNIST.py
@@ -7,7 +7,29 @@ import tensorflow as tf
 # placeholder is a value that we will input when we ask Tensorflow to run a computation
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
-b = tf.Variable(tf.zeros[10])
+b = tf.Variable(tf.zeros([10]))
 
-# one line to define model 
+# one line to define model
 y = tf.nn.softmax(tf.matmul(x,W) + b)
+
+# implement cross entropy (loss)
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+# minimise cross_entropy with a learning rate of 0.5
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+sess = tf.InteractiveSession()
+
+tf.global_variables_initializer().run()
+
+for _ in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+# prediction vs real input
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
